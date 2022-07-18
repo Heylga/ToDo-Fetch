1ca63db74a2421fd9dde87e2177555953e682376
Heylga <oy.golovina@gmail.com>
2022-07-18 10:43:26 +0000

    adding add InputValue and submit Task

---
+++ src/js/component/TodoList.jsx
@@ -0,0 +1,148 @@
+import React, { useState, useEffect } from "react";
+
+const TodoList = () => {
+
+    // HOOKS
+
+
+    const [inputMessage, setInputMessage] = useState("");
+    const [task, setTask] = useState([]);
+    const [counter, setCounter] = useState(0);
+  
+
+    // FETCH
+
+    //RETRIEVE TASKS (GET)
+
+    useEffect(() => {
+        fetch("https://assets.breatheco.de/apis/fake/todos/user/heylga", {
+          method: "GET",
+        }).then((response) => {
+          if (response.status === 404) {
+            createUser("heylga");
+          } else {
+            getList("heylga");
+          }
+        });
+      }, []);
+
+      const getList = (user) => {
+        fetch(`https://assets.breatheco.de/apis/fake/todos/user/${user}`, {
+          method: "GET",
+        })
+          .then((response) => response.json())
+          .then((data) => setTask(data), setCounter(counter + 1));
+      };
+    
+      const createUser = (user) => {
+        fetch(`https://assets.breatheco.de/apis/fake/todos/user/${user}`, {
+          method: "POST",
+          headers: {
+            "Content-type": "application/json",
+          },
+          body: JSON.stringify([]),
+        })
+          .then((response) => response.json())
+          .then((data) => setTask(data));
+      };
+    
+    //ADD/UPDATE AND STORE NEW TASK (PUT)
+    
+      const addTask = (text) => {
+        console.log("text", text);
+        let newTask = [...task, { label: text, done: false }];
+    
+        console.log("here you see the new tasks", newTask);
+    
+        fetch("https://assets.breatheco.de/apis/fake/todos/user/heylga", {
+          method: "PUT",
+          headers: {
+            "Content-type": "application/json",
+          },
+          body: JSON.stringify(newTask),
+        })
+          .then((response) => response.json())
+          .then((data) => {
+            setInputValue("");
+            setTask(newTask);
+            setCounter(counter + 1);
+          });
+      };
+
+    // ADDING TASK WHEN CLICKING ENTER KEY
+
+    const handleKey = (event) => {
+        if (event.key === "Enter" && inputMessage !== " " && inputMessage !== "") {
+                addTask(inputMessage);
+                setCounter(counter + 1);
+                inputMessage("");
+            }
+        };
+    
+        
+    // FUNCTION TO DELETE ONE ITEM
+
+    const DeleteItems = (key) => {
+        let newTask = task.filter((t, i) => i !== key);
+
+        fetch("https://assets.breatheco.de/apis/fake/todos/user/heylga", {
+        method: "PUT",
+        headers: {
+        "Content-type": "application/json",
+        },
+        body: JSON.stringify(newTask),
+        })
+        .then((response) => response.json())
+        .then((data) => {
+            setTask(newTask);
+            setCounter(counter - 1);
+        });
+    };
+
+
+    // FUNCTION TO DELETE ALL ITEMS
+
+    const deleteAll = () => {
+        fetch("https://assets.breatheco.de/apis/fake/todos/user/heylga", {
+            method: "DELETE",
+            headers: {
+            "Content-type": "application/json",
+            },
+        })
+            .then((response) => response.json())
+            .then((data) => {
+                setTask([]);
+                setCounter(0);
+        });
+        };
+
+    return (
+        <div className="ToDo">
+
+            <div className="container-fluid justify-content-center">
+
+                <h1 className="text-center mt-5"> ToDo List </h1>
+
+                <form className="d-flex justify-content-center">
+
+                    <input 
+                    className="list-group-item w-25 fs-4 py-3 shadow p-3"
+                    type="text"
+                    size="72.5"
+                    placeholder="What's need to be done?"
+                    onChange={addTask}
+                    value={inputMessage}
+                    onKeyPressCapture={(e) => handleKey(e)}
+                    />
+
+
+
+                </form>
+
+
+            </div>
+
+        </div>
+    )
+};
+export default TodoList;
